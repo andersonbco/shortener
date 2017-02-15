1a5459937840d7acabebc7422ba42468b71ee27f
Anderson Correa <andersonbcorrea@hotmail.com>
2017-02-15 10:20:27 -0200

    Initial commit of Spring Boot files

---
+++ src/test/java/com/andersonbco/ShortenerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.andersonbco;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShortenerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
